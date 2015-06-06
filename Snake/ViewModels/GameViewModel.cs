e45e01126239ff299cff2d2431115be6ed355618
CodingCoder <codingcoder@danielgilbert.de>
2015-06-06 22:14:03 +0200

    Adds View

--- Snake/ViewModels/GameViewModel.cs
+++ Snake/ViewModels/GameViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Snake.Helper.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace Snake.ViewModels
 {
     public class GameViewModel : BaseViewModel
     {
+        private int _score;
+
+        public int Score
+        {
+            get { return _score; }
+            set
+            {
+                SetProperty<int>(ref _score, value, () => Score);
+            }
+        }
+
     }
 }
